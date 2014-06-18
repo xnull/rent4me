0f52bef3ae3c34fd6787ab4c5b0aea3718908fbe
null <xrw.null@gmail.com>
2014-06-19 02:35:59 +0400

    Replaced the extjs to an angular. Has been added first service

---
+++ realty/realty-services/src/main/java/bynull/realty/services/RentService.java
@@ -0,0 +1,14 @@
+package bynull.realty.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by null on 19.06.14.
+ */
+@Service
+public class RentService {
+
+    public void putForRent(Long userId, Long flatId){
+
+    }
+}
