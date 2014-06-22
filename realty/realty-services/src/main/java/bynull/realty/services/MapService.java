50739aaeee58c86578a8d7a72bfec4ae50bd594b
null <xrw.null@gmail.com>
2014-06-22 18:00:08 +0400

    Last changes

---
+++ realty/realty-services/src/main/java/bynull/realty/services/MapService.java
@@ -0,0 +1,16 @@
+package bynull.realty.services;
+
+/**
+ * Java library for google maps: https://developers.google.com/api-client-library/java/apis/mapsengine/v1
+ * <p/>
+ * Created by null on 22.06.14.
+ */
+public class MapService {
+
+    /**
+     * Create a spot in a map
+     */
+    public void createSpot(){
+
+    }
+}
