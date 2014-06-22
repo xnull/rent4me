4040e5b404ba0b1a520519bec84e31fe1ca25c8c
null <xrw.null@gmail.com>
2014-06-23 00:33:01 +0400

    Preparing to change java version, some improving of gradle scripts, have been added rent service related code on client

---
+++ realty/realty-web/src/main/webapp/view/html/components/rent/rent-service.js
@@ -0,0 +1,9 @@
+/**
+ * User adds his own property to the site
+ * Created by null on 22.06.14.
+ */
+
+/*
+ * 1. User fills fields in UI: address of property, floor number, price, description
+ * 2. User submits the data to the server by json request
+ */
