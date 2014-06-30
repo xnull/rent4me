8c41edf10b51fc033e26e610afc11f475c289905
dionis <dionis.argiri@gmail.com>
2014-06-30 22:39:31 +0300

    Playing with FB app.

--- realty/realty-client/app/app.js
+++ realty/realty-client/app/app.js
@@ -1,12 +1,14 @@
 'use strict';
 
 console.log('Create an angular app module');
-var rentApplication = angular.module('project', ['ngRoute']);
+var rentApplication = angular.module('project', ['ngRoute'/*, 'facebook'*/]);
 
 var RENT_CONTROLLER_NAME = 'RentController';
 var RENT_SERVICE_NAME = 'rentService';
 var LOGIN_CONTROLLER_NAME = 'LoginController';
 var LOGIN_SERVICE_NAME = 'loginService';
+var FB_LOGIN_CONTROLLER_NAME = 'FBLoginController';
+var FB_LOGIN_SERVICE_NAME = 'fbLoginService';
 var REGISTER_CONTROLLER_NAME = 'RegisterController';
 var REGISTER_SERVICE_NAME = 'registerService';
 
@@ -18,8 +20,12 @@ rentApplication.config(function ($routeProvider) {
             templateUrl: 'components/rent/rent-view.html'
         })
         .when('/login', {
-            controller: RENT_CONTROLLER_NAME,
+            controller: LOGIN_CONTROLLER_NAME,
             templateUrl: 'components/login/login-view.html'
+        })
+        .when('/fb_login', {
+            controller: FB_LOGIN_CONTROLLER_NAME,
+            templateUrl: 'components/fb_login/fb_login-view.html'
         })
         .when('/register', {
             controller: REGISTER_CONTROLLER_NAME,
@@ -30,9 +36,18 @@ rentApplication.config(function ($routeProvider) {
         });
 });
 
+/*rentApplication.config(['FacebookProvider', function(FacebookProvider) {
+    // Here you could set your appId through the setAppId method and then initialize
+    // or use the shortcut in the initialize method directly.
+    FacebookProvider.init('270007246518198');
+}]);*/
+
 rentApplication.controller(RENT_CONTROLLER_NAME, rentController);
 rentApplication.controller(LOGIN_CONTROLLER_NAME, loginController);
+rentApplication.controller(FB_LOGIN_CONTROLLER_NAME, fbLoginController);
 rentApplication.controller(REGISTER_CONTROLLER_NAME, registerController);
+
 rentApplication.factory(RENT_SERVICE_NAME, rentService);
 rentApplication.factory(LOGIN_SERVICE_NAME, loginService);
+rentApplication.factory(FB_LOGIN_SERVICE_NAME, fbLoginService);
 rentApplication.factory(REGISTER_SERVICE_NAME, registerService);
