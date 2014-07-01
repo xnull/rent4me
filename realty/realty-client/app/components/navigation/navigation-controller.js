aec9c773ca2b6512fbbc6ad3a2160523b2b7b2f3
dargiri <dionis@hellomobil.ee>
2014-07-01 18:02:07 +0300

    Refactor.

--- realty/realty-client/app/components/navigation/navigation-controller.js
+++ realty/realty-client/app/components/navigation/navigation-controller.js
@@ -1,23 +1,33 @@
 var navigationController = function ($scope, navigationService) {
     console.log("Nav controller");
-//    $scope.navService = navigationService;
-//    console.log("Tab: "+navigationService.tab);
-//    console.log("is selected: "+navigationService.isSelected('home'));
-//    navigationService.isSelected('home');
-//    navigationService.selectTab('register');
-//    navigationService.isSelected('home');
-//    console.log("is selected: "+navigationService.isSelected('home'));
-//    this.tab = navigationService.tab;
-    this.selectTab = navigationService.selectTab;
+
+    this.getTab = navigationService.getTab;
     this.isSelected = navigationService.isSelected;
+    this.selectTab = navigationService.selectTab;
+
+    this.isHome = navigationService.isHome;
+    this.setHome = navigationService.setHome;
+
+    this.isLogin = navigationService.isLogin;
+    this.setLogin = navigationService.setLogin;
+
+    this.isRegister = navigationService.isRegister;
+    this.setRegister = navigationService.setRegister;
+
+    this.isRent = navigationService.isRent;
+    this.setRent = navigationService.setRent;
 };
 
 var navigationService = function () {
     "use strict";
-    this.tab = 'home';
-    var _self = this;
 
-    console.log("Navigation service created");
+    const home = 'home';
+    const register = 'register';
+    const login = 'login';
+    const rent = 'rent';
+
+    this.tab = home;//set default value
+    var _self = this;
 
     return {
         getTab: function() {
@@ -27,7 +37,35 @@ var navigationService = function () {
             _self.tab = setTab;
         },
         isSelected: function (checkTab) {
-            return _self.tab === checkTab;
+            return this.getTab() === checkTab;
+        },
+
+        isHome: function () {
+            return this.isSelected(home);
+        },
+        setHome: function() {
+            this.selectTab(home);
+        },
+
+        isRegister: function () {
+            return this.isSelected(register);
+        },
+        setRegister: function() {
+            this.selectTab(register);
+        },
+
+        isLogin: function () {
+            return this.isSelected(login);
+        },
+        setLogin: function() {
+            this.selectTab(login);
+        },
+
+        isRent: function () {
+            return this.isSelected(rent);
+        },
+        setRent: function() {
+            this.selectTab(rent);
         }
     };
 
