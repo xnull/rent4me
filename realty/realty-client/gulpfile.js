a02f2e149b04be4151284699f363f951ea564f6a
null <xrw.null@gmail.com>
2014-07-02 23:22:31 +0400

    The gulp is added. New task: jsHint it is like a findbug

---
+++ realty/realty-client/gulpfile.js
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+"use strict";
+
+// Includes
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var print = require('gulp-print');
+
+// Lint Task
+gulp.task('lint', function () {
+    return gulp.src('app/components/**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+/**
+ * Print all javascript files
+ */
+gulp.task('print', function () {
+    gulp.src('app/components/**/*.js').pipe(print())
+});
