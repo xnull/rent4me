013893d9f1a034a27c174c1821000a66f706b812
null <xrw.null@gmail.com>
2014-07-13 15:36:21 +0400

    Added a menu in the personal page

--- realty/realty-client/app/components/personal/personal-controller.js
+++ realty/realty-client/app/components/personal/personal-controller.js
@@ -4,6 +4,28 @@
 var personalController = function ($log, $scope, personalService, navigationService) {
     "use strict";
 
+    $scope.menuTabs = [
+        {id: 'home', name: 'Home'},
+        {id: 'settings', name: 'Settings'},
+        {id: 'message', name: 'Messages'}
+    ];
+
+    $scope.currentMenuTabId = $scope.menuTabs[0].id;
+
+    $scope.isActiveMenuTab = function (menuTabId) {
+        $log.debug('Is menu active: ' + menuTabId);
+
+        if ($scope.currentMenuTabId === menuTabId) {
+            return 'active';
+        }
+        return '';
+    };
+
+    $scope.showMenu = function (menuTabId) {
+        $log.info('Show subview: ' + menuTabId);
+        $scope.currentMenuTabId = menuTabId;
+    };
+
     $(function () {
         navigationService.setPersonal();
     });
