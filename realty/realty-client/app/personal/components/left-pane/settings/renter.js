bfa966c0b91f4c4f34a55fd3391ded8a6a4436fb
v.pets <v.pets@home.oorraa.net>
2014-12-18 02:19:04 +0300

    Первые наброски страницы "Я арендатор"

---
+++ realty/realty-client/app/personal/components/left-pane/settings/renter.js
@@ -0,0 +1,23 @@
+/**
+ * Created by null on 18.12.14.
+ */
+var React = require('react');
+
+var Renter = React.createClass({
+    render: function () {
+        return (
+            <div className="col-md-9">
+                <div className="panel">
+                    <div className="panel-body">
+                        <h4>В поисках жилья</h4>
+
+                        <br/>
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+
+module.exports = Renter;
