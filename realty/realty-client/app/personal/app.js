5827c55fd454080ddf07aef6a25b5acffd4ac2ac
v.pets <v.pets@home.oorraa.net>
2015-01-03 21:36:19 +0300

    Отревакторил главную персональную страницу на использование метрик из common

--- realty/realty-client/app/personal/app.js
+++ realty/realty-client/app/personal/app.js
@@ -2,14 +2,18 @@
 var React = require('react');
 
 require('./components/router.js');
+var Utils = require('../shared/common/Utils');
+
+var Analytics = require('../shared/common/analytics');
 var HeaderComponent = require('./components/header/Header.React');
 var LeftPaneComponent = require('./components/left-pane/left-pane.js');
 
 React.render(<HeaderComponent/>, document.getElementById('header'));
 React.render(<LeftPaneComponent/>, document.getElementById('leftPane'));
 
-var Utils = require('../shared/common/Utils');
-
 //export to the window
 window.Utils = Utils;
 window.Cookies = require('../shared/common/Cookies');
+
+Analytics.google();
+Analytics.yandex();
