d08c37e2654e0570d05f9cd85f754df81740d5fd
v.pets <v.pets@home.oorraa.net>
2015-01-06 03:04:54 +0300

    Добавил кнопки поиска с самыми важными параметрами фильтрации, по которым мы должны уметь парсить объявления из соц. сетей

--- realty/realty-client/app/personal/components/left-pane/socialnet/socialForRenter.js
+++ realty/realty-client/app/personal/components/left-pane/socialnet/socialForRenter.js
@@ -40,15 +40,15 @@ module.exports = React.createClass({
         }));
     },
 
-    loadMoreResults: function() {
-        if(this.state.hasMoreSearchResults){
+    loadMoreResults: function () {
+        if (this.state.hasMoreSearchResults) {
             var text = this.state.text;
-            console.log('Searching for text: '+text);
+            console.log('Searching for text: ' + text);
             SocialNetActions.findRenterPosts(text);
         }
     },
 
-    onSearchChange: function(e) {
+    onSearchChange: function (e) {
         var value = e.target.value;
 
         console.log('on search change');
@@ -60,10 +60,10 @@ module.exports = React.createClass({
         }));
     },
 
-    onSubwayChange: function(e) {
+    onSubwayChange: function (e) {
         console.log(e);
         var value = e.target.checked;
-        console.log("With subway new value: "+value);
+        console.log("With subway new value: " + value);
 
         SocialNetActions.changeRenterSearchWithSubway(value);
 
@@ -72,7 +72,7 @@ module.exports = React.createClass({
         }));
     },
 
-    onClear: function(){
+    onClear: function () {
         SocialNetActions.resetRenterSearchState();
         SocialNetActions.changeRenterSearchText(null);
         SocialNetActions.changeRenterSearchWithSubway(false);
@@ -83,10 +83,10 @@ module.exports = React.createClass({
         }));
     },
 
-    onClick: function(){
+    onClick: function () {
         var text = this.state.text;
         var withSubway = this.state.withSubway;
-        console.log('Searching for text: '+text);
+        console.log('Searching for text: ' + text);
 
         SocialNetActions.resetRenterSearchState();
         SocialNetActions.changeRenterSearchText(text);
@@ -94,13 +94,13 @@ module.exports = React.createClass({
         SocialNetActions.findRenterPosts(text, withSubway);
     },
 
-    render: function() {
+    render: function () {
         var items = this.state.posts || [];
         var hasMoreResults = this.state.hasMoreSearchResults || false;
         var text = this.state.text || '';
         var withSubWay = this.state.withSubway || false;
 
-        console.log('with subway? '+withSubWay);
+        console.log('with subway? ' + withSubWay);
 
         return (
             <div className="col-md-9">
@@ -109,37 +109,128 @@ module.exports = React.createClass({
                     <div className="panel-body">
                         <h4>Объявления о сдаче</h4>
 
-
                         <form className="form-horizontal" role="form">
-                            <div className="form-group">
-                                <label className="col-md-2 control-label">Поиск</label>
-                                <div className="col-md-10">
-                                    <input type="text" className="form-control"
-                                        value={text}
-                                        placeholder="Введите текст для поиска по объявлениям"
-                                        onChange={this.onSearchChange} ></input>
+                            <div className='row'>
+                                <div className='col-md-6'>
+                                    <div className="col-md-2">
+                                        <label className="control-label">Комнат</label>
+                                    </div>
+                                    <div className="col-md-10">
+                                        <div className="btn-group" data-toggle="buttons">
+                                            <label className="btn btn-default">
+                                                <input type="checkbox">1</input>
+                                            </label>
+                                            <label className="btn btn-default">
+                                                <input type="checkbox">2</input>
+                                            </label>
+                                            <label className="btn btn-default">
+                                                <input type="checkbox">3</input>
+                                            </label>
+                                            <label className="btn btn-default">
+                                                <input type="checkbox">4+</input>
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className='col-md-6'>
+                                    <div className="control-group">
+                                        <div className="dropdown">
+                                            <button className="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
+                                                Срок аренды
+                                                <span className="caret"></span>
+                                            </button>
+                                            <ul className="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+                                                <li role="presentation">
+                                                    <a role="menuitem" tabindex="-1" href="#" style={{focus: {backgroundColor: '#000000'}}}>Посуточно</a>
+                                                </li>
+                                                <li role="presentation">
+                                                    <a role="menuitem" tabindex="-1" href="#">Месяц</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <br/>
+
+                            <div className='row'>
+                                <div className='col-md-6'>
+                                    <div className="col-md-2">
+                                        <label className="control-label">Цена</label>
+                                    </div>
+                                    <div className="col-md-10">
+                                        <div className="col-md-6">
+                                            <input type="text" className="form-control col-md-3" placeholder="От"/>
+                                        </div>
+
+                                        <div className="col-md-6">
+                                            <input type="text" className="form-control col-md-3" placeholder="До"/>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className='col-md-6'>
+                                    <div className="input-group">
+                                        <input type="text" className="form-control" aria-label="..." placeholder="Станция метро"/>
+                                        <div className="input-group-btn">
+                                            <button type="button" className="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Выбрать
+                                                <span className="caret"></span>
+                                            </button>
+                                            <ul className="dropdown-menu dropdown-menu-right" role="menu">
+                                                <li>
+                                                    <a href="#">Станция цветной бульвар</a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">Новослободская</a>
+                                                </li>
+                                                <li>
+                                                    <a href="#">1905 года</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
 
-                            <div className="form-group">
-                                <label className="col-md-2 control-label">
-                                    С метро
-                                </label>
-                                <div className="col-md-10">
-                                    <input
-                                        type="checkbox"
-                                        onChange={this.onSubwayChange}
-                                        checked={withSubWay}
-                                    />
+
+                            <hr/>
+
+                            <div className='row'>
+                                <div className='col-md-12'>
+                                    <div className="form-group">
+                                        <label className="col-md-2 control-label">Поиск</label>
+                                        <div className="col-md-10">
+                                            <input type="text" className="form-control"
+                                                value={text}
+                                                placeholder="Адрес, улица, район, метро"
+                                                onChange={this.onSearchChange} ></input>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
+
+                                <div className='col-md-6'>
+                                    <div className="form-group">
+                                        <label className="col-md-2 control-label">
+                                            С метро
+                                        </label>
+                                        <div className="col-md-10">
+                                            <input
+                                                type="checkbox"
+                                                onChange={this.onSubwayChange}
+                                                checked={withSubWay}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
 
 
-                            <div className="col-md-offset-6 col-md-3">
-                                <a className="btn btn-danger center-block" onClick={this.onClear}>Очистить</a>
-                            </div>
-                            <div className="col-md-3">
-                                <a className="btn btn-primary center-block" onClick={this.onClick}>Поиск</a>
+                                <div className="col-md-offset-6 col-md-3">
+                                    <a className="btn btn-danger center-block" onClick={this.onClear}>Очистить</a>
+                                </div>
+                                <div className="col-md-3">
+                                    <a className="btn btn-primary center-block" onClick={this.onClick}>Поиск</a>
+                                </div>
                             </div>
                         </form>
                     </div>
