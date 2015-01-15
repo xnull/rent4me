8c4a1e3017d6271a5b6eacecb37015a984a3e34b
dionis <dionis.argiri@gmail.com>
2015-01-15 05:32:47 +0200

    Upgrade scalate to 1.7.o

--- realty/realty-admin/realty-admin-web-app-launcher/src/main/java/name/dargiri/Launcher.java
+++ realty/realty-admin/realty-admin-web-app-launcher/src/main/java/name/dargiri/Launcher.java
@@ -5,7 +5,7 @@ package name.dargiri;
  */
 public class Launcher {
     private static final String DEFAULT_PATH = "src/main/webapp";
-    private static final String MULTI_MODULE_DEFAULT_PATH = "realty-admin/web/src/main/webapp";
+    private static final String MULTI_MODULE_DEFAULT_PATH = "realty-admin/realty-admin-web/src/main/webapp";
 
     public static void main(String[] args) throws Exception {
         new BaseLauncher(Launcher.class.getResource("Launcher.class"), Launcher.class, DEFAULT_PATH, MULTI_MODULE_DEFAULT_PATH, "admin").doStart("8888");
