656e77e07f0c454700479f7311c8fd49690dcd09
dionis <dionis.argiri@gmail.com>
2015-01-21 01:16:26 +0200

    Fix.

--- realty/realty-services/src/main/java/bynull/realty/components/text/RoomCountParser.java
+++ realty/realty-services/src/main/java/bynull/realty/components/text/RoomCountParser.java
@@ -2,6 +2,7 @@ package bynull.realty.components.text;
 
 import bynull.realty.utils.TextUtils;
 import com.google.common.collect.ImmutableSet;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Set;
@@ -10,6 +11,7 @@ import java.util.regex.Pattern;
 /**
  * Created by dionis on 20/01/15.
  */
+@Slf4j
 public class RoomCountParser {
 
     public static final int FLAGS = Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.UNICODE_CASE;
@@ -33,11 +35,13 @@ public class RoomCountParser {
         );
 
         oneRoomApartmentPatterns = ImmutableSet.of(
-                Pattern.compile("((.*)(одн\\S{1,3})(.{0,5})(комнатн\\S*)(.*))", FLAGS),
-                Pattern.compile("((.*)(1(\\-)?\\S{1,3})(.{0,5})(комнатн\\S*)(.*))", FLAGS),
-                Pattern.compile("((.*)(1(\\-)?\\S{1,3})(.{0,5})(ушк\\S*)(.*))", FLAGS),
-                Pattern.compile("((.*)(1((\\-)?)((.){0,5})к\\S{0,5})(.{0,10})(кв\\S*)(.*))", FLAGS),
-                Pattern.compile("((.*)(1((\\-)?)((.){0,5})комн\\S{0,5})(.{0,10})(кв\\S*)(.*))", FLAGS)
+                Pattern.compile("((.*)(одн([^\\s\\.,]){1,3})(.{0,5})(комнатн([^\\s\\.,])*)(.*))", FLAGS),
+                Pattern.compile("((.*)(одн([^\\s\\.,]){1,5})(.{0,5})(к(([^\\s\\.,]){0,8})(.{0,2})(кв([^\\s\\.,])*))(.*))", FLAGS),
+                Pattern.compile("((.*)(1(\\-)?([^\\s\\.,]){1,3})(.{0,5})(к(([^\\s\\.,]){0,8})(.{0,2})(кв([^\\s\\.,])*))(.*))", FLAGS),
+                Pattern.compile("((.*)(1(\\-)?([^\\s\\.,]){1,3})(.{0,5})(комнатн([^\\s\\.,])*)(.*))", FLAGS),
+                Pattern.compile("((.*)(1(\\-)?([^\\s\\.,]){1,3})(.{0,5})(ушк([^\\s\\.,])*)(.*))", FLAGS),
+                Pattern.compile("((.*)(1((\\-)?)((.){0,5})к([^\\s\\.,]){0,5})(.{0,10})(кв([^\\s\\.,])*)(.*))", FLAGS),
+                Pattern.compile("((.*)(1((\\-)?)((.){0,5})комн([^\\s\\.,]){0,5})(.{0,10})(кв([^\\s\\.,])*)(.*))", FLAGS)
         );
 
         twoRoomApartmentStems = ImmutableSet.of(
@@ -48,12 +52,14 @@ public class RoomCountParser {
         );
 
         twoRoomApartmentPatterns = ImmutableSet.of(
-                Pattern.compile("((.*)(дв\\S{1,5})(.{0,5})(комнат\\S*)(.*))", FLAGS),
-                Pattern.compile("((.*)(2(\\-)?\\S{1,5})(.{0,5})(комнатн\\S*)(.*))", FLAGS),
-                Pattern.compile("((.*)(2(\\-)?\\S{1,5})(.{0,5})(ушк\\S*)(.*))", FLAGS),
-                Pattern.compile("((.*)(2((\\-)?)((.){0,5})к\\S{0,5})(.{0,10})(кв\\S*)(.*))", FLAGS),
-                Pattern.compile("((.*)(2((\\-)?)((.){0,5})к(а|у)\\S{0,5})(.*))", FLAGS),
-                Pattern.compile("((.*)(2((\\-)?)((.){0,5})комн\\S{0,5})(.{0,10})(кв\\S*)(.*))", FLAGS)
+                Pattern.compile("((.*)(дв([^\\s\\.,]){1,5})(.{0,5})(комнат([^\\s\\.,])*)(.*))", FLAGS),
+                Pattern.compile("((.*)(дв([^\\s\\.,]){0,5})(.{0,5})(к(([^\\s\\.,]){0,8})(.{0,2})(кв([^\\s\\.,])*))(.*))", FLAGS),
+                Pattern.compile("((.*)(2(\\-)?(([^\\s\\.,]){0,3}))(.{0,5})(комнатн([^\\s\\.,])*)(.*))", FLAGS),
+                Pattern.compile("((.*)(2(\\-)?([^\\s\\.,]){0,2}х)(.{0,5})(к(([^\\s\\.,]){0,8})(.{0,2})(кв([^\\s\\.,])*))(.*))", FLAGS),
+                Pattern.compile("((.*)(2(\\-)?([^\\s\\.,]){0,2}х)(.{0,5})(ушк([^\\s\\.,])*)(.*))", FLAGS),
+                Pattern.compile("((.*)(2((\\-)?([^\\s\\.,]){0,3})((.){0,5})к([^\\s\\.,]){0,5})(.{0,10})(кв([^\\s\\.,])*)(.*))", FLAGS),
+                Pattern.compile("((.*)(2((\\-)?([^\\s\\.,]){0,3})((.)?)к(а|у)([^\\s\\.,]){0,5})(.*))", FLAGS),
+                Pattern.compile("((.*)(2((\\-)?([^\\s\\.,]){0,2}х)((.){0,5})комн([^\\s\\.,]){0,5})(.{0,10})(кв([^\\s\\.,])*)(.*))", FLAGS)
         );
 
         threeRoomApartmentStems = ImmutableSet.of(
@@ -66,15 +72,17 @@ public class RoomCountParser {
         );
 
         threeRoomApartmentPatterns = ImmutableSet.of(
-                Pattern.compile("((.*)(тр(е|ё)х\\S{0,5})(.{0,5})(комнат\\S*)(.*))", FLAGS),
-                Pattern.compile("((.*)(тр(е|ё)\\S{0,5})(.{0,5})(комнат\\S*)(.*))", FLAGS),
-                Pattern.compile("((.*)(три\\S{0,5})(.{0,5})(комнат\\S*)(.*))", FLAGS),
-                Pattern.compile("((.*)(тр(е|ё)шк\\S*)(.*))", FLAGS),
-                Pattern.compile("((.*)(3(\\-)?\\S{0,5})(.{0,5})(комнатн\\S*)(.*))", FLAGS),
-                Pattern.compile("((.*)(3(\\-)?\\S{0,5})(.{0,5})(ушк\\S*)(.*))", FLAGS),
-                Pattern.compile("((.*)(3(\\-)?\\S{0,5})((.)?)(((е|ё)?)шк\\S*)(.*))", FLAGS),
-                Pattern.compile("((.*)(3((\\-)?)((.){0,5})к\\S{0,5})(.{0,10})(кв\\S*)(.*))", FLAGS),
-                Pattern.compile("((.*)(3((\\-)?)((.){0,5})комн\\S{0,5})(.{0,10})(кв\\S*)(.*))", FLAGS)
+                Pattern.compile("((.*)(тр(е|ё)х([^\\s\\.,]){0,5})(.{0,5})(комнат([^\\s\\.,])*)(.*))", FLAGS),
+                Pattern.compile("((.*)(тр(е|ё)([^\\s\\.,]){0,5})(.{0,5})(комнат([^\\s\\.,])*)(.*))", FLAGS),
+                Pattern.compile("((.*)(тр(е|ё)([^\\s\\.,]){0,5})(.{0,5})(к(([^\\s\\.,]){0,8})(.{0,2})(кв([^\\s\\.,])*))(.*))", FLAGS),
+                Pattern.compile("((.*)(три([^\\s\\.,]){0,5})(.{0,5})(комнат([^\\s\\.,])*)(.*))", FLAGS),
+                Pattern.compile("((.*)(тр(е|ё)шк([^\\s\\.,])*)(.*))", FLAGS),
+                Pattern.compile("((.*)(3(\\-)?([^\\s\\.,]){0,5})(.{0,5})(к(([^\\s\\.,]){0,8})(.{0,2})(кв([^\\s\\.,])*))(.*))", FLAGS),
+                Pattern.compile("((.*)(3(\\-)?([^\\s\\.,]){0,5})(.{0,5})(комнатн([^\\s\\.,])*)(.*))", FLAGS),
+                Pattern.compile("((.*)(3(\\-)?([^\\s\\.,]){0,5})(.{0,5})(ушк([^\\s\\.,])*)(.*))", FLAGS),
+                Pattern.compile("((.*)(3(\\-)?([^\\s\\.,]){0,5})((.)?)(((е|ё)?)шк([^\\s\\.,])*)(.*))", FLAGS),
+                Pattern.compile("((.*)(3((\\-)?)((.){0,5})к([^\\s\\.,]){0,5})(.{0,10})(кв([^\\s\\.,])*)(.*))", FLAGS),
+                Pattern.compile("((.*)(3((\\-)?)((.){0,5})комн([^\\s\\.,]){0,5})(.{0,10})(кв([^\\s\\.,])§*)(.*))", FLAGS)
         );
 
     }
@@ -92,11 +100,13 @@ public class RoomCountParser {
         // кейсы 3-ех комнатных квартир
         for (String stem : threeRoomApartmentStems) {
             if (text.contains(stem)) {
+                log.info("3-room Matched by stem [{}]", stem);
                 return 3;
             }
         }
         for (Pattern pattern : threeRoomApartmentPatterns) {
             if (pattern.matcher(text).matches()) {
+                log.info("3-room Matched by pattern [{}]", pattern.pattern());
                 return 3;
             }
         }
@@ -104,11 +114,13 @@ public class RoomCountParser {
         // кейсы 2-ух комнатной квартиры
         for (String stem : twoRoomApartmentStems) {
             if (text.contains(stem)) {
+                log.info("2-room Matched by stem [{}]", stem);
                 return 2;
             }
         }
         for (Pattern pattern : twoRoomApartmentPatterns) {
             if (pattern.matcher(text).matches()) {
+                log.info("2-room Matched by pattern [{}]", pattern.pattern());
                 return 2;
             }
         }
@@ -116,11 +128,13 @@ public class RoomCountParser {
         // кейсы однокомнатной квартиры
         for (String stem : oneRoomApartmentStems) {
             if (text.contains(stem)) {
+                log.info("1-room Matched by stem [{}]", stem);
                 return 1;
             }
         }
         for (Pattern pattern : oneRoomApartmentPatterns) {
             if (pattern.matcher(text).matches()) {
+                log.info("1-room Matched by pattern [{}]", pattern.pattern());
                 return 1;
             }
         }
