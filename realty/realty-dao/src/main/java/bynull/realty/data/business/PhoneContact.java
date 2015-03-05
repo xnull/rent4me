bd27628eda26fedd40e365146cdf66b1d82a7a48
Dionis Argiri <dionis.argiri@gmail.com>
2015-03-05 17:52:02 +0200

    Fix.

--- realty/realty-dao/src/main/java/bynull/realty/data/business/PhoneContact.java
+++ realty/realty-dao/src/main/java/bynull/realty/data/business/PhoneContact.java
@@ -3,10 +3,13 @@ package bynull.realty.data.business;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 /**
  * Created by dionis on 3/5/15.
  */
+@Entity
+@DiscriminatorValue(Contact.DbValue.PHONE_DB_VALUE)
 public class PhoneContact extends Contact {
     @Embedded
     private PhoneNumber phoneNumber;
