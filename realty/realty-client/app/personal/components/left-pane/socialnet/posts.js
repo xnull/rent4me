833288ea64e512b1ab098d82b50b40387aaf8bfb
v.pets <v.pets@home.oorraa.net>
2015-03-09 21:06:53 +0300

    Рефакторинг постов, выделил некоторые переменные в виде компонентов часть 2

--- realty/realty-client/app/personal/components/left-pane/socialnet/posts.js
+++ realty/realty-client/app/personal/components/left-pane/socialnet/posts.js
@@ -120,6 +120,94 @@ var ContactInfo = React.createClass({
     }
 });
 
+var ImagePreviews = React.createClass({
+    getFirstImage: function () {
+        var item = this.props.item;
+        var firstImage;
+
+        if (item.data_source == 'INTERNAL') {
+            firstImage = item.photos ? _.first(item.photos.map(photo => {
+                return (
+                    <img className="media-object" width="160"  src={photo.small_thumbnail_url}/>
+                );
+            })) : null;
+        } else {
+            firstImage = item.img_urls ? _.first(item.img_urls.map(function (photo) {
+                return (
+                    <img className="media-object" width="160"  src={photo}/>
+                );
+            })) : null;
+        }
+    },
+
+    render: function () {
+        var withoutImage = (<img className="media-object"  src="http://placehold.it/160"/>);
+        var firstImage = this.getFirstImage();
+        return (
+            <div>
+                <div className="thumbnail pull-left" style={{marginBottom: 0}}>
+                {firstImage ? firstImage : withoutImage}
+                </div>
+            </div>
+        )
+    }
+});
+
+var HeaderBlock = React.createClass({
+    render: function () {
+        var item = this.props.item;
+        var priceInfo = (
+            <div>
+                Цена: {item.rental_fee ? accounting.formatNumber(item.rental_fee, 0, " ") : 'не распознано'}
+            </div>
+        );
+
+        var addressInfo = (
+            <div>
+                {item.address && item.address.formatted_address ? item.address.formatted_address : ''}
+            </div>
+        );
+
+        var footer = (
+            <div>
+                Добавлено: {moment(item.created).format("lll")}
+            </div>
+        );
+
+        return (
+            <div className="panel-heading" style={{backgroundColor: 'rgba(207, 207, 207, 0.27)', borderBottom: 'none'}}>
+                <div className='row'>
+                    <div className='col-md-2'>
+                        <p>{priceInfo}</p>
+                    </div>
+                    <div className='col-md-6'>
+                        <p>{addressInfo}</p>
+                    </div>
+                    <div className='col-md-4'>
+                        <p>{footer}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+});
+
+var Message = React.createClass({
+    render: function () {
+        var item = this.props.item;
+        return (
+            <div className="panel-body" style={{padding: '15px 15px 0 15px'}}>
+                <div className="row">
+                    <div className="col-md-12 well"
+                        style={{boxShadow: 'none', backgroundColor: 'rgba(250, 250, 250, 1)', marginBottom: 0}}
+                        dangerouslySetInnerHTML={{__html: Utils.nl2br(item.description)}}>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+});
+
 var Post = React.createClass({
 
     getInitialState: function () {
@@ -146,85 +234,12 @@ var Post = React.createClass({
     render: function () {
         var item = this.props.item || {};
 
-        var firstImage;
-
-        if (item.data_source == 'INTERNAL') {
-            firstImage = item.photos ? _.first(item.photos.map(photo => {
-                return (
-                    <img className="media-object" width="160"  src={photo.small_thumbnail_url}/>
-                );
-            })) : null;
-        } else {
-            firstImage = item.img_urls ? _.first(item.img_urls.map(function (photo) {
-                return (
-                    <img className="media-object" width="160"  src={photo}/>
-                );
-            })) : null;
-        }
-
-        var priceInfo = (
-            <div>
-                Цена: {item.rental_fee ? accounting.formatNumber(item.rental_fee, 0, " ") : 'не распознано'}
-            </div>
-        );
-
-        var addressInfo = (
-            <div>
-                {item.address && item.address.formatted_address ? item.address.formatted_address : ''}
-            </div>
-        );
-
-        var imagePreviews = firstImage ? (
-            <div>
-                <div className="thumbnail pull-left" style={{marginBottom: 0}}>
-                {firstImage}
-                </div>
-            </div>
-        ) : (
-            <div className="thumbnail pull-left" style={{marginBottom: 0}}>
-                <img className="media-object"  src="http://placehold.it/160"/>
-            </div>
-        );
-
-        var footer = (
-            <div>
-                Добавлено: {moment(item.created).format("lll")}
-            </div>
-        );
-
-        var headerBlock = (
-            <div className="panel-heading" style={{backgroundColor: 'rgba(207, 207, 207, 0.27)', borderBottom: 'none'}}>
-                <div className='row'>
-                    <div className='col-md-2'>
-                        <p>{priceInfo}</p>
-                    </div>
-                    <div className='col-md-6'>
-                        <p>{addressInfo}</p>
-                    </div>
-                    <div className='col-md-4'>
-                        <p>{footer}</p>
-                    </div>
-                </div>
-            </div>
-        );
-
-        var message = (
-            <div className="panel-body" style={{padding: '15px 15px 0 15px'}}>
-                <div className="row">
-                    <div className="col-md-12 well"
-                        style={{boxShadow: 'none', backgroundColor: 'rgba(250, 250, 250, 1)', marginBottom: 0}}
-                        dangerouslySetInnerHTML={{__html: Utils.nl2br(item.description)}}>
-                    </div>
-                </div>
-            </div>
-        );
-
         return (
             <div className='panel panel-info'>
-                    {headerBlock}
+                <HeaderBlock item={item}/>
                 <div className="panel-body">
                     <div className="media">
-                       {imagePreviews}
+                        <ImagePreviews item={item} />
                         <div className="media-body">
                             <div className="row">
                                 <MetroPreviews metros={item.metros}/>
@@ -233,7 +248,7 @@ var Post = React.createClass({
                             </div>
                         </div>
                     </div>
-                    {message}
+                    <Message item={item}/>
                 </div>
             </div>
         );
