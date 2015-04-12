3783f1fb192e3ce8541d1e01477e7bd079c60b7d
bynull <xrw.null@gmail.com>
2015-04-12 23:14:56 +0300

    hide legal component in small devices

--- realty/realty-client/app/personal/components/left-pane/legal.js
+++ realty/realty-client/app/personal/components/left-pane/legal.js
@@ -7,23 +7,27 @@ var Utils = require('rent4meUtil');
 module.exports = React.createClass({
     render: function () {
         return (
-            <div className="panel panel-default" style={Utils.inactiveUi}>
-                <div className="panel-body">
-                    <h4>Квартирный вопрос</h4>
+            <div className='hidden-xs hidden-sm'>
+                <div className="panel panel-default" style={Utils.inactiveUi}>
+                    <div className="panel-body">
+                        <h4>Квартирный вопрос</h4>
 
-                    <div className="bs-component">
-                        <div className="list-group">
-                            <a href="#" className="list-group-item">
-                                <h4 className="list-group-item-heading">Типовой договор</h4>
-                                <p className="list-group-item-text">Типовой договор аренды недвижимости</p>
-                            </a>
-                            <a href="#" className="list-group-item">
-                                <h4 className="list-group-item-heading">Рассылки</h4>
+                        <div className="bs-component">
+                            <div className="list-group">
+                                <a href="#" className="list-group-item">
+                                    <h4 className="list-group-item-heading">Типовой договор</h4>
 
-                                <p className="list-group-item-text">Подпишитесь на рассылки чтобы немедленно узнавать
-                                    о новых объявлениях
-                                </p>
-                            </a>
+                                    <p className="list-group-item-text">Типовой договор аренды недвижимости</p>
+                                </a>
+                                <a href="#" className="list-group-item">
+                                    <h4 className="list-group-item-heading">Рассылки</h4>
+
+                                    <p className="list-group-item-text">Подпишитесь на рассылки чтобы немедленно
+                                        узнавать
+                                        о новых объявлениях
+                                    </p>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
