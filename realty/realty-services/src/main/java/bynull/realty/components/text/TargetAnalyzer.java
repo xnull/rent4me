6689f36f23201beec11fdc3d62d191c0d448b9d5
Dionis Argiri <dionis.argiri@gmail.com>
2015-04-27 17:49:30 +0300

    Bug #104 - enhancement.

--- realty/realty-services/src/main/java/bynull/realty/components/text/TargetAnalyzer.java
+++ realty/realty-services/src/main/java/bynull/realty/components/text/TargetAnalyzer.java
@@ -12,7 +12,7 @@ import java.util.List;
  */
 @Component
 public class TargetAnalyzer {
-    private static final List<String> LESSOR_TARGETED_KEYWORDS = ImmutableList.of("сниму", "снимаю", "снять", "снял", "возьму", "возьмет", "взял", "взять", "снимем", "снимет");
+    private static final List<String> LESSOR_TARGETED_KEYWORDS = ImmutableList.of("сниму", "снимаю", "снять", "снял", "возьму", "возьмет", "взял", "взять", "снимем", "снимет", "снимут");
     private static final List<String> RENTER_TARGETED_KEYWORDS = ImmutableList.of("сдаю", "сдам", "отдам", "отдаю", "отдается", "отдает",
             "сдается", "подселю", "сдаем", "сдает",
             //слова долбоеб стайл
