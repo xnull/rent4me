b52750a45b7da82785dda371b6e9972df2440da1
Dionis Argiri <dionis.argiri@gmail.com>
2015-05-02 17:25:26 +0300

    Fix admin - v4.

--- realty/realty-admin/realty-admin-web/src/main/java/name/dargiri/web/stubs/ChatMessageOnlineNotifierStub.java
+++ realty/realty-admin/realty-admin-web/src/main/java/name/dargiri/web/stubs/ChatMessageOnlineNotifierStub.java
@@ -2,12 +2,14 @@ package name.dargiri.web.stubs;
 
 import bynull.realty.components.api.ChatMessageUsersOnlineNotifier;
 import bynull.realty.dto.ChatMessageDTO;
+import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 
 /**
  * Created by dionis on 5/2/15.
  */
+@Component
 public class ChatMessageOnlineNotifierStub implements ChatMessageUsersOnlineNotifier {
     @Override
     public void sendMessagesToParticipants(ChatMessageDTO chatMessageDTO) {
