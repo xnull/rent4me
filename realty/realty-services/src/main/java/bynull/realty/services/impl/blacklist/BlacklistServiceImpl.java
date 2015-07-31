35425402d0ba26b536983f6c0934500724311ba2
null <xrw.null@gmail.com>
2015-07-31 03:29:55 +0300

    #issue 107 Описал алгоритм действий с черным списком. Определился с вопросом связывания идентификаторов

--- realty/realty-services/src/main/java/bynull/realty/services/impl/blacklist/BlacklistServiceImpl.java
+++ realty/realty-services/src/main/java/bynull/realty/services/impl/blacklist/BlacklistServiceImpl.java
@@ -46,14 +46,34 @@ public class BlacklistServiceImpl {
 
         Optional<Blacklist> optBlackInfo = Optional.ofNullable(blacklistRepo.findByApartmentId(apartmentId));
 
-        if (!optBlackInfo.isPresent()) {
-            Optional<ApartmentDTO> apartment = apartmentService.find(apartmentId);
+        /**
+         * Для identifications таблицы надо будет реализовать алгоритм связывания идентификаторов.
+         * Например, мы создали 2 записи в ids таблице. В первой записи лежит телефон во второй почта.
+         * Через некоторое время мы обнаруживаем, что телефон и почта принадлежат одному и тому-же лицу.
+         * Вопрос: что делать?
+         * Варианты:
+         *  1. Объединить записи, удалив одну из них или создав 3-ю запись, удалив 2 предыдущие. Путь опасен
+         *  2. Создаем таблицу ids_ids отношение многие ко многим, в таблице 2 столбца каждый указывает на ids. Когда
+         *     связь между идентификаторами будет обнаружена мы добавим запись в ids_ids. И по этой таблице сможем
+         *     находить все связи всех идентификаторов.
+         */
 
-            Blacklist bl = new Blacklist();
-            bl.setApartmentId(apartment.get().getId());
-            switch (apartment.get().getDataSource()) {
-                case INTERNAL:
-                    //check that identification exists if not then create it
+        if (!optBlackInfo.isPresent()) {
+            Optional<ApartmentDTO> optApartment = apartmentService.find(apartmentId);
+
+            /**
+             * 1. Находим все идентификаторы с которыми связаны данные апартаменты
+             * 2. Ищем все идентификаторы с ids_ids и ids таблице и если обнаружились новые связи идентификаторов
+             *    через объявление, то связываем все имеющиеся идентификаторы путем обновления таблиц ids_ids и ids
+             * 3. добавляем ссылку на ids в черный список
+             */
+            if (optApartment.isPresent()){
+                Blacklist bl = new Blacklist();
+                bl.setApartmentId(optApartment.get().getId());
+
+                switch (optApartment.get().getDataSource()) {
+                    case INTERNAL:
+                        //check that identification exists if not then create it
                     /*idRepo.findByUserId();
                     idRepo.findByVkId();
                     idRepo.findByFbId();
@@ -62,25 +82,30 @@ public class BlacklistServiceImpl {
 
                     //если нашли ид хоть по одному из параметров, то добавляем его в черный список
                     if(found){
-                        bl.setIdentificationId(apartment.getOwner().getId());
+                        bl.setIdentificationId(optApartment.getOwner().getId());
                         ...
                     }else{
                         создать новый ид и вписать туда имеющиеся знания о нарушителе
                         idRepo.save(id);
                     }*/
-                    break;
-                case FACEBOOK:
-                    //get fb user id
-                    break;
-                case VKONTAKTE:
-                    //get vk user
-                    break;
-            }
+                        break;
+                    case FACEBOOK:
+                        //get fb user id
+                        break;
+                    case VKONTAKTE:
+                        //get vk user
+                        break;
+                }
 
+            }else {
+                /**
+                 * вываливаем в лог ошыбку, о том что произошла неведомая херня
+                 */
+            }
 
             //blacklistRepo.save()
         } else {
-
+            //алгоритм тот же что и выше, только надо создать запись в блэк таблице
         }
     }
 }
