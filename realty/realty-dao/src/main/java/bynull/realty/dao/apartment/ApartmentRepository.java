9900edf2200adf8b8cf64247dfdb0e46314831ca
dionis <dionis.argiri@gmail.com>
2015-10-19 16:57:55 +0300

    Hopefully fixed.

--- realty/realty-dao/src/main/java/bynull/realty/dao/apartment/ApartmentRepository.java
+++ realty/realty-dao/src/main/java/bynull/realty/dao/apartment/ApartmentRepository.java
@@ -26,15 +26,14 @@ public interface ApartmentRepository extends JpaRepository<Apartment, Long>, Apa
             "where a.published=true " +
             "and upper(a.country_code)=upper(:countryCode) " +
             //check here that it's in bounding box
-            "and st_setsrid(st_makebox2d(ST_GeomFromText( concat('SRID=4326;POINT('," +
+            "and st_setsrid(" +
+            "st_makebox2d(St_Point( " +
                 ":lng_low," +
-                "' '," +
                 ":lat_low," +
-            "')')), ST_GeomFromText( concat('SRID=4326;POINT('," +
+            "), St_Point( " +
             ":lng_high," +
-            "' '," +
-            ":lat_high," +
-            "')'))), 4326)" +
+            ":lat_high" +
+            ")), 4326)" +
             " ~ a.location" +
             " " +
             "order by a.location <-> ST_GeomFromText( concat('SRID=4326;POINT(',:lng,' ',:lat,')') ) limit :limit offset :offset", nativeQuery = true)
--- realty/realty-embedded-launcher-rest/src/main/java/bynull/realty/LauncherRest.java
+++ realty/realty-embedded-launcher-rest/src/main/java/bynull/realty/LauncherRest.java
@@ -15,7 +15,7 @@ public final class LauncherRest {
 
         new BaseLauncher(LauncherRest.class.getResource("LauncherRest.class"), LauncherRest.class, Arrays.asList(
                 new BaseLauncher.HandlerDesc("src/main/webapp", "realty-web/src/main/webapp", "")
-                , new BaseLauncher.HandlerDesc("build-js", "realty-client/build-js", "dev")
+//                , new BaseLauncher.HandlerDesc("build-js", "realty-client/build-js", "dev")
 //                , new BaseLauncher.HandlerDesc("app/start", "realty-client/app/start", "start")
 //                , new BaseLauncher.HandlerDesc("app/personal", "realty-client/app/personal", "personal")
 //                , new BaseLauncher.HandlerDesc("node_modules", "realty-client/node_modules", "node_modules")
