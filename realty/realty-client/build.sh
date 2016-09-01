c0cda38cfdd4bc92946b1c455e890d68c1841387
Dionis Argiri <dionis.argiri@swedbank.ee>
2016-09-01 16:47:41 +0300

    Create bash build script

---
+++ realty/realty-client/build.sh
@@ -0,0 +1,10 @@
+rm -r dist* > /dev/null 2>&1
+mkdir dist
+./webpack.minimize.sh \
+	&& cp -r index.html dist/ \
+	&& cp -r build dist/ \
+	&& cp -r css dist/ \
+	&& cp -r external_js dist/ \
+	&& cp -r fonts dist/ \
+	&& cp -r images dist/ \
+	&& cd dist && tar -cvzf app.tar.gz *
